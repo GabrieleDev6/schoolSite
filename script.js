682d46fef32ae1cf9825ec45d6c47d632f8db1f7
GabrieleDev6 <113252079+GabrieleDev6@users.noreply.github.com>
2022-12-08 10:40:28 +0100

    create script.js file

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $().timelinr({
+        orientation: 'vertical',
+        issuesSpeed: 300,
+        datesSpeed: 100,
+        arrowKeys: 'true',
+        startAt: 3
+    })
+});
+
+
